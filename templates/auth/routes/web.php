51c03c06f8ed79ca55d3c9d6be8bbec910a06a0c
Enzo Innocenzi <enzo@innocenzi.dev>
2020-08-29 18:36:25 +0200

    refactor: remove short closure
    Use `Route::view` instead, so it can be used in PHP < 7.4 without a change. Fixes #3.

--- templates/auth/routes/web.php
+++ templates/auth/routes/web.php
@@ -16,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', fn () => view('welcome'));
+Route::view('/', 'welcome');
 Route::get('/home', 'HomeController@index')->name('home');
