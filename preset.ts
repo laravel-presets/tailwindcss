95f55d6169911acaaa3a17c7bd8c1105cb3b9db5
Enzo Innocenzi <enzo@innocenzi.dev>
2021-07-12 14:14:43 +0200

    feat: remove Mix support

--- preset.ts
+++ preset.ts
@@ -1,35 +1,22 @@
 import { color, Preset } from 'apply';
 
 Preset.setName('Tailwind CSS for Laravel');
-Preset.option('install', true);
-// Preset.option('auth', true);
+Preset.option('install', false);
 
 Preset.group((preset) => {
   preset.extract('default');
-  // preset.extract('auth').ifOption('auth');
 }).withTitle('Extracting templates...');
 
 Preset.edit('.gitignore')
   .withTitle(`Updating ${color.magenta('.gitignore')}...`)
-  .addAfter('node_modules', ['/public/build', '/public/mix-manifest.json']);
+  .addAfter('node_modules', ['/public/build'])
 
 Preset.group((preset) => {
   preset
     .editNodePackages()
-    .addDev('laravel-mix', '^6.0.0-beta.14')
-    .addDev('tailwindcss', '^2.0.1')
-    .addDev('autoprefixer', '^10')
-    .addDev('postcss', '^8.1.8')
-    .addDev('vue-template-compiler', '^2.6.12');
-  preset.editNodePackages().merge({
-    scripts: {
-      dev: 'npm run development',
-      development: 'npx mix',
-      watch: 'npx mix watch',
-      prod: 'npm run production',
-      production: 'npx mix -p',
-    },
-  });
+    .addDev('tailwindcss', '^2.2.4')
+    .addDev('autoprefixer', '^10.3.0')
+    .addDev('postcss', '^8.3.5')
   preset.installDependencies().ifOption('install');
 }).withTitle('Installing dependencies...');
 
--- templates/default/tailwind.config.js
+++ templates/default/tailwind.config.js
@@ -1,34 +1,21 @@
 const { fontFamily } = require('tailwindcss/defaultTheme');
-const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: [
-    './app/**/*.php',
-    './resources/**/*.html',
-    './resources/**/*.js',
-    './resources/**/*.jsx',
-    './resources/**/*.ts',
-    './resources/**/*.php',
-    './resources/**/*.vue',
-  ],
-  darkMode: 'media', // or 'media' or 'class'
-  theme: {
-    colors: {
-      white: '#ffffff',
-      black: '#000000',
-      gray: colors.coolGray,
-      indigo: colors.indigo,
-      red: colors.rose,
-      yellow: colors.amber,
-    },
-    extend: {
-      fontFamily: {
-        sans: ['Inter', ...fontFamily.sans],
-      },
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-};
+	mode: 'jit',
+	purge: [
+		'./resources/**/*.blade.php',
+		'./resources/**/*.{js,ts,vue}',
+	],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Inter', ...fontFamily.sans],
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+}
